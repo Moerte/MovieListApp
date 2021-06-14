570652029a4c974f8cf254c2e9bda5ce2e2a6f70
Moerte86 <nunoliveira86@gmail.com>
2021-06-14 18:11:02 +0100

    First commit

--- Movies List/ViewController.swift
+++ Movies List/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtLogin: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func loginAction(_ sender: Any) {
+        
+        if(txtLogin.text == "Nuno" && txtPassword.text == "1234"){
+            performSegue(withIdentifier: "ScreenTwo", sender: self)
+        }else{
+            
+            let alert = UIAlertController(title: "Alert", message: "Wrong Credentials", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+            
+    }
 }
 
